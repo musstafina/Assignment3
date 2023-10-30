15c2d4f373eedcee0765b1494e9c38d75761aa94
Nargiz Merlenkyzy <merlenqyzy@mail.ru>
2023-10-31 03:08:04 +0600

    functions

--- project/order_manager/order_manager.go
+++ project/order_manager/order_manager.go
@@ -19,3 +19,22 @@ func GetOrderManager() *OrderManager {
 	}
 	return instance
 }
+
+func (om *OrderManager) AddOrder(orderID int, order *Order) {
+	om.mu.Lock()
+	om.orders[orderID] = order
+	om.mu.Unlock()
+}
+
+func (om *OrderManager) GetOrder(orderID int) (*Order, bool) {
+	om.mu.Lock()
+	order, found := om.orders[orderID]
+	om.mu.Unlock()
+	return order, found
+}
+
+func (om *OrderManager) RemoveOrder(orderID int) {
+	om.mu.Lock()
+	delete(om.orders, orderID)
+	om.mu.Unlock()
+}
